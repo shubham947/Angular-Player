eb6746fb27eaab603fa45a55f46115e7ad09fffa
Shubham Kumar <shubhamkumar94733@gmail.com>
2022-09-03 23:16:18 +0530

    Updated demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'AngularPlayer';
-
+export class AppComponent implements OnInit {
+  title = 'Angular Player';
+  playFrom?:number;
+  vol?:number;
+  
   videoSrc = "https://cdn.videvo.net/videvo_files/video/free/2015-02/large_watermarked/Pine_Trees_in_Sunlight_01_Videvo_preview.mp4";
+  videoList = [
+    "https://cdn.videvo.net/videvo_files/video/free/2015-02/large_watermarked/Pine_Trees_in_Sunlight_01_Videvo_preview.mp4",
+    "https://cdn.videvo.net/videvo_files/video/free/2016-05/large_watermarked/482952308_preview.mp4",
+    "https://cdn.videvo.net/videvo_files/video/free/2016-01/large_watermarked/Countryside_16_1_Videvo_preview.mp4",
+    "https://cdn.videvo.net/videvo_files/video/free/2014-12/large_watermarked/Sqirrel_Eats_Close_preview.mp4"
+  ];
+  
+  ngOnInit(): void {
+    
+  }
+
+  track = 0;
+  onEnd(ended:boolean) {
+    this.videoSrc = this.videoList[(++this.track) % this.videoList.length];
+  }
   
 }
